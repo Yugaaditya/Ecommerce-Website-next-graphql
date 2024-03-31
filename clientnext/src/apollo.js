1eb7540e53cb16d5cceeb2338fd1949496c5ce99
Yugaaditya Achhara <f20201692@goa.bits-pilani.ac.in>
2024-03-31 22:52:47 +0530

    API Endpoint Fix

--- clientnext/src/apollo.js
+++ clientnext/src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: "https://ecommerce-website-next-graphql.vercel.app/",
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
